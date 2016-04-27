3e29b009caa9e1011d4c45ec32648624c7fbc3f0
Anders <andersenghoej@gmail.com>
2016-04-27 15:52:17 +0200

    first proto

---
+++ next.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+  console.log("test");
+  first = getQueryVariable("first");
+  second = getQueryVariable("second");
+  third = getQueryVariable("third");
+  console.log(first);
+  replaceText(first);
+  replaceText(second);
+  replaceText(third);
+};
+
+function getQueryVariable(variable){
+   var query = window.location.search.substring(1);
+   var vars = query.split("&");
+   for (var i=0;i<vars.length;i++) {
+           var pair = vars[i].split("=");
+           if(pair[0] == variable){return pair[1];}
+   }
+   return(false);
+}
+
+function replaceText(variable){
+  var para = document.createElement("p");
+  var node = document.createTextNode(variable);
+  para.appendChild(node);
+  var element = document.getElementById("div1");
+  element.appendChild(para);
+}
